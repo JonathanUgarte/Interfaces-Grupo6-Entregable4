5a627a74eae4e50c8092c4bdae557e9accb84990
JonathanUgarte <jona.ugarte10@gmail.com>
2024-11-21 11:21:09 -0300

    fixes

--- js/animacionOpciones.js
+++
@@ -1,7 +0,0 @@
-const menuItems = document.querySelectorAll('.menu-item');
-
-menuItems.forEach((item, index) => {
-  setTimeout(() => {
-    item.classList.add('show');
-  }, index * 300); // 300ms de retraso entre cada elemento
-});
